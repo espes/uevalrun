39a62930263217e60f73b3b1b65424dab6ffa756
ptspts <ptspts@5875f506-4f57-11de-a3de-f74ccfcb021d>
2010-11-28 10:44:44 +0000

    added copying to precompiled
    git-svn-id: http://pts-mini-gpl.googlecode.com/svn/trunk/uevalrun@137 5875f506-4f57-11de-a3de-f74ccfcb021d

--- Makefile
+++ Makefile
@@ -9,9 +9,9 @@ CC = i386-uclibc-gcc
 
 CC_DETECT = $(shell if test "$$CC"; then echo "$$CC"; elif i386-uclibc-gcc -v >/dev/null 2>&1; then echo i386-uclibc-gcc -static; else echo gcc -static; fi)
 
-ALL = uevalrun uevalrun.guestinit uevalrun.rootfs.minix.img
+ALL = uevalrun uevalrun.guestinit minihalt uevalrun.rootfs.minix.img
 
-.PHONY: all clean run_sys run_mini_sys
+.PHONY: all clean run_sys run_mini_sys run_gcc_sys
 
 all:
 	$(MAKE) CC='$(CC_DETECT)' $(ALL)
@@ -41,3 +41,6 @@ run_gcc_sys:
 	./busybox dd if=/dev/zero of=uevalrun.rootfs.gcxtmp.minix.img bs=2000K count=1
 	./busybox mkfs.minix -n 30 -i 20 uevalrun.rootfs.gcxtmp.minix.img
 	./uevalrun.linux.uml con=null ssl=null con0=fd:0,fd:1 mem=60M ubda=uevalrun.rootfs.gcc.minix.img ubdb=uevalrun.rootfs.gcxtmp.minix.img init=/sbin/minihalt rw ubde=hello.c
+
+precompile: uevalrun uevalrun.guestinit minihalt examples/xcat
+	cp $^ precompiled/
--- examples/run.sh
+++ examples/run.sh
@@ -17,6 +17,10 @@ elif test "$1" = php; then
   ../uevalrun -M 32 -T 3 -E 20 -s scat.php -t answer.in -e answer.exp
 elif test "$1" = perl; then
   ../uevalrun -M 32 -T 3 -E 20 -s scat.pl -t answer.in -e answer.exp
+elif test "$1" = perl.ok; then
+  ../uevalrun -M 32 -T 3 -E 20 -s scatok.pl -t answer.in -e answer.exp
+elif test "$1" = c; then
+  ../uevalrun -M 3 -T 9 -E 20 -s scatc   -C 2 -N 32 -U 10 -t answer.in -e answer.scatc.exp
 elif test "$1" = gcc; then
   ../uevalrun -M 3 -T 9 -E 20 -s scat.c  -C 2 -N 32 -U 10 -t answer.in -e answer.exp
 elif test "$1" = gxx; then
@@ -26,8 +30,8 @@ elif test "$1" = long; then
   ../uevalrun -M 32 -T 3 -E 20 -s scatlong.pl -t answer.in -e long.exp
 elif test "$#" = 1; then
   $CC -W -Wall -s -O2 -static -o xcat xcat.c
-  ../uevalrun -M 3 -T 3 -E 20 -s xcat -t answer.in -e answer.bad.exp
+  ../uevalrun -M 26 -T 3 -E 20 -s xcat -t answer.in -e answer.bad.exp
 else
   $CC -W -Wall -s -O2 -static -o xcat xcat.c
-  ../uevalrun -M 3 -T 3 -E 20 -s xcat -t answer.in -e answer.exp
+  ../uevalrun -M 26 -T 3 -E 20 -s xcat -t answer.in -e answer.exp
 fi
